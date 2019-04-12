ab96c416328e5d8e5a197f37474c25a022b9de46
Brian <brian@quantrocket.com>
2019-04-12 16:40:18 -0400

    update sample code for quantrocket 1.6

--- qval/qval.py
+++ qval/qval.py
@@ -22,21 +22,21 @@ class USStockCommission(PerShareCommission):
 class QuantitativeValue(Moonshot):
     """
     Value strategy modeled on Alpha Architect's QVAL ETF.
-    
-    Intended to be run with Sharadar fundamentals and prices. 
-    
+
+    Intended to be run with Sharadar fundamentals and prices.
+
     Strategy rules:
-    
+
     1. Universe selection
       a. Starting universe: all NYSE stocks
       b. Exclude financials, ADRs, REITs
-      c. Liquidity screen: select top N percent of stocks by dollar 
+      c. Liquidity screen: select top N percent of stocks by dollar
          volume (N=60)
-    2. Apply value screen: select cheapest N percent of stocks by 
+    2. Apply value screen: select cheapest N percent of stocks by
        enterprise multiple (EV/EBIT) (N=10)
     3. Rank by quality: of the value stocks, select the N percent
        with the highest quality, as ranked by Piotroski F-Score (N=50)
-    4. Apply equal weights    
+    4. Apply equal weights
     5. Rebalance portfolio quarterly
     """
 
@@ -50,11 +50,11 @@ class QuantitativeValue(Moonshot):
     VALUE_TOP_N_PCT = 10
     QUALITY_TOP_N_PCT = 50
     REBALANCE_INTERVAL = "Q"
-    MASTER_DOMAIN = "sharadar" # use "sharadar" with Sharadar prices, use "main" with IB prices 
+    MASTER_DOMAIN = "sharadar" # use "sharadar" with Sharadar prices, use "main" with IB prices
     COMMISSION_CLASS = USStockCommission
 
     def prices_to_signals(self, prices):
-        
+
         # Step 1.c: get a mask of stocks with adequate dollar volume
         closes = prices.loc["Close"]
         volumes = prices.loc["Volume"]
@@ -62,12 +62,12 @@ class QuantitativeValue(Moonshot):
         dollar_volume_ranks = avg_dollar_volumes.rank(axis=1, ascending=False, pct=True)
         have_adequate_dollar_volumes = dollar_volume_ranks <= (self.DOLLAR_VOLUME_TOP_N_PCT/100)
 
-        # Step 2. Apply value screen: select cheapest N percent of stocks by 
+        # Step 2. Apply value screen: select cheapest N percent of stocks by
         # enterprise multiple (EV/EBITDA) (N=10)
         fundamentals = get_sharadar_fundamentals_reindexed_like(
-            closes, 
-            fields=["EVEBIT", "EBIT"], 
-            dimension="ART", 
+            closes,
+            fields=["EVEBIT", "EBIT"],
+            dimension="ART",
             domain=self.MASTER_DOMAIN)
         enterprise_multiples = fundamentals.loc["EVEBIT"]
         ebits = fundamentals.loc["EBIT"]
@@ -76,25 +76,21 @@ class QuantitativeValue(Moonshot):
         # Only apply rankings to stocks with adequate dollar volume
         value_ranks = enterprise_multiples.where(have_adequate_dollar_volumes).rank(axis=1, ascending=True, pct=True)
         are_value_stocks = value_ranks <= (self.VALUE_TOP_N_PCT/100)
-        
+
         # Step 3: Rank by quality: of the value stocks, select the N percent
         # with the highest quality, as ranked by Piotroski F-Score (N=50)
         f_scores = self.get_f_scores(closes)
         # Rank the value stocks by F-Score
         quality_ranks = f_scores.where(are_value_stocks).rank(axis=1, ascending=False, pct=True)
         long_signals = quality_ranks <= (self.QUALITY_TOP_N_PCT/100)
-        
-        # Save holdings to results for inspection
-        symbols = prices.loc["Symbol"].reindex(closes.index, method="ffill")
-        self.save_to_results("Holdings", symbols.where(long_signals))
-        
+
         return long_signals.astype(int)
-    
+
     def get_f_scores(self, closes):
-        
+
         # Step 1: query relevant indicators
         fundamentals = get_sharadar_fundamentals_reindexed_like(
-            closes, 
+            closes,
             domain=self.MASTER_DOMAIN,
            dimension="ART", # As-reported trailing twelve month reports
            fields=[
@@ -106,7 +102,6 @@ class QuantitativeValue(Moonshot):
                "SHARESWA", # Outstanding shares
                "GROSSMARGIN", # Gross margin
                "ASSETTURNOVER", # Asset turnover
-               "REPORTPERIOD"
            ])
         return_on_assets = fundamentals.loc["ROA"]
         total_assets = fundamentals.loc["ASSETS"]
@@ -116,12 +111,17 @@ class QuantitativeValue(Moonshot):
         shares_out = fundamentals.loc["SHARESWA"]
         gross_margins = fundamentals.loc["GROSSMARGIN"]
         asset_turnovers = fundamentals.loc["ASSETTURNOVER"]
-        
-        # Step 2: many Piotroski F-score components compare current to previous 
+
+        # Step 2: many Piotroski F-score components compare current to previous
         # values, so get DataFrames of previous values
-        
+
         # Step 2.a: get a boolean mask of the first day of each newly reported fiscal
         # period
+        fundamentals = get_sharadar_fundamentals_reindexed_like(
+            closes,
+            domain=self.MASTER_DOMAIN,
+            dimension="ART", # As-reported trailing twelve month reports
+            fields=["REPORTPERIOD"])
         fiscal_periods = fundamentals.loc["REPORTPERIOD"]
         are_new_fiscal_periods = fiscal_periods != fiscal_periods.shift()
 
@@ -136,9 +136,9 @@ class QuantitativeValue(Moonshot):
         previous_shares_out = shares_out.shift().where(are_new_fiscal_periods).fillna(method="ffill")
         previous_gross_margins = gross_margins.shift().where(are_new_fiscal_periods).fillna(method="ffill")
         previous_asset_turnovers = asset_turnovers.shift().where(are_new_fiscal_periods).fillna(method="ffill")
-        
-        # Step 3: calculate F-Score components; each resulting component is a DataFrame 
-        # of booleans 
+
+        # Step 3: calculate F-Score components; each resulting component is a DataFrame
+        # of booleans
         have_positive_return_on_assets = return_on_assets > 0
         have_positive_operating_cash_flows = operating_cash_flows > 0
         have_increasing_return_on_assets = return_on_assets > previous_return_on_assets
@@ -161,15 +161,15 @@ class QuantitativeValue(Moonshot):
             + have_increasing_gross_margins.astype(int)
             + have_increasing_asset_turnovers.astype(int)
         )
-        
+
         self.save_to_results("FScore", f_scores)
         return f_scores
-    
+
     def signals_to_target_weights(self, signals, prices):
         # Step 4: equal weights
         daily_signal_counts = signals.abs().sum(axis=1)
         weights = signals.div(daily_signal_counts, axis=0).fillna(0)
-        
+
         # Step 5: Rebalance quarterly
         # Resample daily to quarterly, taking the last day's signal
         # For pandas offset aliases, see https://pandas.pydata.org/pandas-docs/stable/timeseries.html#offset-aliases
@@ -184,13 +184,12 @@ class QuantitativeValue(Moonshot):
         return weights.shift()
 
     def positions_to_gross_returns(self, positions, prices):
-        
+
         closes = prices.loc["Close"]
         position_ends = positions.shift()
-        
+
         # The return is the security's percent change over the period,
         # multiplied by the position.
         gross_returns = closes.pct_change() * position_ends
-        
+
         return gross_returns
-    
